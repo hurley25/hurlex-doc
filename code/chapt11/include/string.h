c3dab5d73d75dd05f2deb44fa797c798e5c5f8a4
hurley <liuhuan1992@gmail.com>
2017-04-09 15:25:05 +0800

    字符串函数移动到头文件，添加static，修复strcmp的问题，感谢@UKeeySDis 指出

--- code/chapt11/include/string.h
+++ code/chapt11/include/string.h
@@ -21,18 +21,73 @@
 
 #include "types.h"
 
-void memcpy(uint8_t *dest, const uint8_t *src, uint32_t len);
+static inline void memcpy(uint8_t *dest, const uint8_t *src, uint32_t len)
+{
+	for (; len != 0; len--) {
+		*dest++ = *src++;
+	}
+}
 
-void memset(void *dest, uint8_t val, uint32_t len);
+static inline void memset(void *dest, uint8_t val, uint32_t len)
+{
+	uint8_t *dst = (uint8_t *)dest;
 
-void bzero(void *dest, uint32_t len);
+	for ( ; len != 0; len--) {
+		*dst++ = val;
+	}
+}
 
-int strcmp(const char *str1, const char *str2);
+static inline void bzero(void *dest, uint32_t len)
+{
+	memset(dest, 0, len);
+}
 
-char *strcpy(char *dest, const char *src);
+static inline int strcmp(const char *str1, const char *str2)
+{
+    while (*str1 && *str2 && *str1 == *str2) {
+        str1++;
+        str2++;
+    }
 
-char *strcat(char *dest, const char *src);
+    return *str1 - *str2;
+}
 
-int strlen(const char *src);
+static inline char *strcpy(char *dest, const char *src)
+{
+	char *tmp = dest;
+
+	while (*src) {
+	      *dest++ = *src++;
+	}
+
+	*dest = '\0';
+
+	return tmp;
+}
+
+static inline char *strcat(char *dest, const char *src)
+{
+	char *cp = dest;
+
+	while (*cp) {
+	      cp++;
+	}
+
+	while ((*cp++ = *src++))
+	      ;
+
+	return dest;
+}
+
+static inline int strlen(const char *src)
+{
+	const char *eos = src;
+
+        while (*eos++)
+	      ;
+
+	return (eos - src - 1);
+}
 
 #endif 	// INCLUDE_STRING_H_
+
